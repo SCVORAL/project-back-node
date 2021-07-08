c984e8a419e8c738218734b86062e6802c62525a
SCVORAl <systemked@mail.ru>
2021-07-08 15:25:02 +0300

    fix

--- settings/db.js
+++ settings/db.js
@@ -8,7 +8,7 @@ const Sequelize = require("sequelize")
 //   }
 // });
 
-const sequelize = new Sequelize( process.env.DATABASE_URL {
+const sequelize = new Sequelize( process.env.DATABASE_URL, {
   dialect: "mysql",
 });
 
